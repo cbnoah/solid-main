bfd51d24ed5464319514bbdb002053333b33f583
cnoah <noah.charrinbourrat@ynov.com>
2026-01-15 19:54:53 +0100

    feat: old mandatory implementation have been removed

--- isp/src/ImprimanteSimple.java
+++ isp/src/ImprimanteSimple.java
@@ -3,12 +3,4 @@ public class ImprimanteSimple implements Machine {
     public void print() {
         System.out.println("Impression en cours...");
     }
-
-    public void scan() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void fax() {
-        throw new UnsupportedOperationException();
-    }
 }
